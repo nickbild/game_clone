f5b5a847b6abe5ccb22e373e1a20af8b8f8f3ba5
Nick Bild <nick.bild@gmail.com>
2025-07-08 20:38:26 -0400

    Support continuing training.

--- train.py
+++ train.py
@@ -6,8 +6,8 @@ import datetime
 import glob
 
 
-MSE_THRESHOLD = 1
-LOAD_SAVED_DATA = False
+MSE_THRESHOLD = 0.5
+CONTINUE_TRAINING_MODEL = "pong.2025-07-08_2.keras"
 
 
 class stopCallback(keras.callbacks.Callback): 
@@ -21,100 +21,99 @@ train_x = []
 train_y = []
 data = []
 
-if not LOAD_SAVED_DATA:
-    print ("Reading in game data ({0})...".format(datetime.datetime.now()))
 
-    # Read the game data into memory.
-    for filename in glob.glob("game_data_*.txt"):
-        with open(filename, "r") as f:
-            for line in f:
-                if line.strip() == "":
-                    continue
-                paddle1_pos, paddle2_pos, ball_x, ball_y, paddle1_vel, paddle2_vel = map(int, line.split())
-                data.append([paddle1_pos, paddle2_pos, ball_x, ball_y, paddle1_vel, paddle2_vel])        
+print ("Reading in game data ({0})...".format(datetime.datetime.now()))
 
-        # Create the training data structure.
-        step = 1
-        if filename.endswith("game_data_10.txt"):
-            step = 3
-
-        for pos in range(0, len(data)-2, step):
-            paddle1_pos_1 = data[pos][0] 
-            paddle2_pos_1 = data[pos][1]
-            ball_x_1 = data[pos][2]
-            ball_y_1 = data[pos][3]
-            paddle1_vel_1 = data[pos][4]
-            paddle2_vel_1 = data[pos][5]
-
-            paddle1_pos_2 = data[pos+1][0] 
-            paddle2_pos_2 = data[pos+1][1]
-            ball_x_2 = data[pos+1][2]
-            ball_y_2 = data[pos+1][3]
-            paddle1_vel_2 = data[pos+1][4]
-            paddle2_vel_2 = data[pos+1][5]
-            
-            paddle1_pos_3 = data[pos+2][0] 
-            paddle2_pos_3 = data[pos+2][1]
-            ball_x_3 = data[pos+2][2]
-            ball_y_3 = data[pos+2][3]
-            paddle1_vel_3 = data[pos+2][4]
-            paddle2_vel_3 = data[pos+2][5]
-
-            # Ignore breaks in data collection that introduce artificially large changes in screen element positions.
-            if abs(paddle1_pos_1 - paddle1_pos_2) > 8 or abs(paddle1_pos_2 - paddle1_pos_3) > 8:
+# Read the game data into memory.
+for filename in glob.glob("game_data_*.txt"):
+    with open(filename, "r") as f:
+        for line in f:
+            if line.strip() == "":
                 continue
-            if abs(paddle2_pos_1 - paddle2_pos_2) > 8 or abs(paddle2_pos_2 - paddle2_pos_3) > 8:
-                continue
+            paddle1_pos, paddle2_pos, ball_x, ball_y, paddle1_vel, paddle2_vel = map(int, line.split())
+            data.append([paddle1_pos, paddle2_pos, ball_x, ball_y, paddle1_vel, paddle2_vel])        
 
-            # LSTM:
-            train_x.append([[paddle1_pos_1, paddle2_pos_1, ball_x_1, ball_y_1, paddle1_vel_1, paddle2_vel_1], [paddle1_pos_2, paddle2_pos_2, ball_x_2, ball_y_2, paddle1_vel_2, paddle2_vel_2]])
-            # Dense:
-            # train_x.append([paddle1_pos_1, paddle2_pos_1, ball_x_1, ball_y_1, paddle1_vel_1, paddle2_vel_1, paddle1_pos_2, paddle2_pos_2, ball_x_2, ball_y_2, paddle1_vel_2, paddle2_vel_2])
-            train_y.append([paddle1_pos_3, paddle2_pos_3, ball_x_3, ball_y_3])
+    # Create the training data structure.
+    step = 1
+    if filename.endswith("game_data_10.txt"):
+        step = 3
 
-        data = [] # Free up memory.
-    
-    print("Done reading in game data ({0}).".format(datetime.datetime.now()))
+    for pos in range(0, len(data)-2, step):
+        paddle1_pos_1 = data[pos][0] 
+        paddle2_pos_1 = data[pos][1]
+        ball_x_1 = data[pos][2]
+        ball_y_1 = data[pos][3]
+        paddle1_vel_1 = data[pos][4]
+        paddle2_vel_1 = data[pos][5]
 
-# Build the model.
-model = keras.Sequential()
-model.add(keras.layers.InputLayer(shape=(2, 6,)))
-model.add(keras.layers.LSTM(160, activation='relu'))
+        paddle1_pos_2 = data[pos+1][0] 
+        paddle2_pos_2 = data[pos+1][1]
+        ball_x_2 = data[pos+1][2]
+        ball_y_2 = data[pos+1][3]
+        paddle1_vel_2 = data[pos+1][4]
+        paddle2_vel_2 = data[pos+1][5]
+        
+        paddle1_pos_3 = data[pos+2][0] 
+        paddle2_pos_3 = data[pos+2][1]
+        ball_x_3 = data[pos+2][2]
+        ball_y_3 = data[pos+2][3]
+        paddle1_vel_3 = data[pos+2][4]
+        paddle2_vel_3 = data[pos+2][5]
 
-# model.add(keras.layers.LSTM(32, return_sequences=True, activation="relu"))
-# model.add(keras.layers.LSTM(32, activation="relu"))
-# model.add(keras.layers.Dense(64, activation="relu"))
+        # Ignore breaks in data collection that introduce artificially large changes in screen element positions.
+        if abs(paddle1_pos_1 - paddle1_pos_2) > 8 or abs(paddle1_pos_2 - paddle1_pos_3) > 8:
+            continue
+        if abs(paddle2_pos_1 - paddle2_pos_2) > 8 or abs(paddle2_pos_2 - paddle2_pos_3) > 8:
+            continue
 
-# model.add(keras.layers.Conv1D(128, 2, activation='relu', padding='same'))
-# model.add(keras.layers.MaxPooling1D(2))
-# model.add(keras.layers.Conv1D(128, 2, activation='relu', padding='same'))
-# model.add(keras.layers.Flatten())
-# model.add(keras.layers.Dense(64, activation='relu'))
-# model.add(keras.layers.MaxPooling1D(2, padding="same"))
-# model.add(keras.layers.LSTM(96, return_sequences=True, activation="relu"))
-# model.add(keras.layers.LSTM(64, activation="relu"))
+        # LSTM:
+        train_x.append([[paddle1_pos_1, paddle2_pos_1, ball_x_1, ball_y_1, paddle1_vel_1, paddle2_vel_1], [paddle1_pos_2, paddle2_pos_2, ball_x_2, ball_y_2, paddle1_vel_2, paddle2_vel_2]])
+        # Dense:
+        # train_x.append([paddle1_pos_1, paddle2_pos_1, ball_x_1, ball_y_1, paddle1_vel_1, paddle2_vel_1, paddle1_pos_2, paddle2_pos_2, ball_x_2, ball_y_2, paddle1_vel_2, paddle2_vel_2])
+        train_y.append([paddle1_pos_3, paddle2_pos_3, ball_x_3, ball_y_3])
 
-model.add(keras.layers.Dense(64, activation='relu'))
-model.add(keras.layers.Dense(4))
+    data = [] # Free up memory.
 
-# Compile the model.
-optimizer = keras.optimizers.Adam(0.0001)
-model.compile(optimizer=optimizer,
-              loss='mse',
-              metrics=['mse', 'mae'])
+print("Done reading in game data ({0}).".format(datetime.datetime.now()))
+
+if CONTINUE_TRAINING_MODEL != "":
+    print("Loading saved model: {0}".format(CONTINUE_TRAINING_MODEL))
+    model = keras.models.load_model(CONTINUE_TRAINING_MODEL)
+
+else:
+    # Build the model.
+    model = keras.Sequential()
+    model.add(keras.layers.InputLayer(shape=(2, 6,)))
+    model.add(keras.layers.LSTM(160, activation='relu'))
+
+    # model.add(keras.layers.LSTM(32, return_sequences=True, activation="relu"))
+    # model.add(keras.layers.LSTM(32, activation="relu"))
+    # model.add(keras.layers.Dense(64, activation="relu"))
+
+    # model.add(keras.layers.Conv1D(128, 2, activation='relu', padding='same'))
+    # model.add(keras.layers.MaxPooling1D(2))
+    # model.add(keras.layers.Conv1D(128, 2, activation='relu', padding='same'))
+    # model.add(keras.layers.Flatten())
+    # model.add(keras.layers.Dense(64, activation='relu'))
+    # model.add(keras.layers.MaxPooling1D(2, padding="same"))
+    # model.add(keras.layers.LSTM(96, return_sequences=True, activation="relu"))
+    # model.add(keras.layers.LSTM(64, activation="relu"))
+
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dense(4))
+
+    # Compile the model.
+    optimizer = keras.optimizers.Adam(0.0001)
+    model.compile(optimizer=optimizer,
+                loss='mse',
+                metrics=['mse', 'mae'])
 
 # Print the model summary.
 model.summary()
 
 # Prepare the training data.
-if not LOAD_SAVED_DATA:
-    train_x = np.asarray(train_x).astype('float32')
-    train_y = np.asarray(train_y).astype('float32')
-    np.save('train_x.npy', train_x)
-    np.save('train_y.npy', train_y)
-else:
-    train_x = np.load('train_x.npy')
-    train_y = np.load('train_y.npy')
+train_x = np.asarray(train_x).astype('float32')
+train_y = np.asarray(train_y).astype('float32')
 
 # Shuffle the training data.
 # This mixes up the data chosen for the validation set,
