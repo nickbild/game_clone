882b83d6b536c7dcf403e0dd4470e0bedb6f02ae
Nick Bild <nick.bild@gmail.com>
2025-07-18 17:22:12 -0400

    Script to 'play' the model.

--- neural_pong.py
+++ neural_pong.py
@@ -47,8 +47,8 @@ window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('NeuralPong')
 
 # Set the initial game state.
-past_positions = [[300, 300, 403, 297, 0, 0], [300, 300, 406, 294, 0, 0]]
-past_positions = np.asarray(past_positions).astype('float32').reshape(1, 2, 6)
+past_positions = [[300, 300, 403, 297, 0, 0], [300, 300, 406, 294, 0, 0], [300, 300, 409, 291, 0, 0], [300, 300, 412, 288, 0, 0], [300, 300, 415, 285, 0, 0]]
+past_positions = np.asarray(past_positions).astype('float32').reshape(1, 5, 6)
 
 # Load the saved model.
 model = keras.models.load_model("pong.keras")
@@ -69,12 +69,15 @@ while True:
 
     # Rotate the latest prediction into the past positions array.
     past_positions[0][0] = past_positions[0][1]
-    past_positions[0][1] = np.append(np.rint(new_prediction[0]), [paddle1_vel, paddle2_vel])
+    past_positions[0][1] = past_positions[0][2]
+    past_positions[0][2] = past_positions[0][3]
+    past_positions[0][3] = past_positions[0][4]
+    past_positions[0][4] = np.append(np.rint(new_prediction[0]), [paddle1_vel, paddle2_vel])
 
     # Update the display.
     window.fill(BLACK)
-    pygame.draw.circle(window, RED, [int(past_positions[0][1][2]), int(past_positions[0][1][3])], BALL_RADIUS, 0)
-    pygame.draw.polygon(window, RED, [[0, int(past_positions[0][1][0]) - HALF_PAD_HEIGHT], [0, int(past_positions[0][1][0]) + HALF_PAD_HEIGHT], [9, int(past_positions[0][1][0]) + HALF_PAD_HEIGHT], [9, int(past_positions[0][1][0]) - HALF_PAD_HEIGHT]], 0)
-    pygame.draw.polygon(window, RED, [[790, int(past_positions[0][1][1]) - HALF_PAD_HEIGHT], [790, int(past_positions[0][1][1]) + HALF_PAD_HEIGHT], [799, int(past_positions[0][1][1]) + HALF_PAD_HEIGHT], [799, int(past_positions[0][1][1]) - HALF_PAD_HEIGHT]], 0)
+    pygame.draw.circle(window, RED, [int(past_positions[0][4][2]), int(past_positions[0][4][3])], BALL_RADIUS, 0)
+    pygame.draw.polygon(window, RED, [[0, int(past_positions[0][4][0]) - HALF_PAD_HEIGHT], [0, int(past_positions[0][4][0]) + HALF_PAD_HEIGHT], [9, int(past_positions[0][4][0]) + HALF_PAD_HEIGHT], [9, int(past_positions[0][4][0]) - HALF_PAD_HEIGHT]], 0)
+    pygame.draw.polygon(window, RED, [[790, int(past_positions[0][4][1]) - HALF_PAD_HEIGHT], [790, int(past_positions[0][4][1]) + HALF_PAD_HEIGHT], [799, int(past_positions[0][4][1]) + HALF_PAD_HEIGHT], [799, int(past_positions[0][4][1]) - HALF_PAD_HEIGHT]], 0)
     pygame.display.update()
     # fps.tick(60)
